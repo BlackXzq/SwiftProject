c301309844785746676b012bbc1f1421ccc18d00
xuzhanqiang <xuzhanqiang@anji-allways.com>
2018-01-11 13:05:02 +0800

    add基础工程

---
+++ AJPlusOCR/AJPlusOCR/LoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  AJPlusOCR
+//
+//  Created by Black on 2018/1/11.
+//  Copyright © 2018年 Black. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "登录"
+
+        self.view.backgroundColor = UIColor.red
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
